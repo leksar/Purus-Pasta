a097fdd861b1a5674a066548ff3c903c806f5919
puruscor <herra.melkor@gmail.com>
2016-06-29 18:38:29 +0300

    Fix issues related to localization refactoring

--- src/SatiateTip.java
+++ src/SatiateTip.java
@@ -23,11 +23,11 @@ class SatiateTip extends Tip {
     }
 
     public BufferedImage longtip() {
-        String satiateStr = Resource.getLocString(Resource.l10nLabel, "Satiate ");
+        String satiateStr = Resource.getLocString(Resource.BUNDLE_LABEL, "Satiate ");
         BufferedImage sat = Text.render(satiateStr).img;
         int h = sat.getHeight();
         BufferedImage ico = PUtils.convolvedown(((Image) ((Resource) this.icon.get()).layer(Resource.imgc)).img, new Coord(h, h), Constipations.tflt);
-        String byStr = Resource.getLocString(Resource.l10nLabel, " by $col[255,128,128]{%d%%}");
+        String byStr = Resource.getLocString(Resource.BUNDLE_LABEL, " by $col[255,128,128]{%d%%}");
         BufferedImage by = RichText.render(String.format(byStr, new Object[]{Integer.valueOf((int) Math.round((1.0D - this.val) * 100.0D))}), 0, new Object[0]).img;
         return catimgsh(0, new BufferedImage[]{sat, ico, by});
     }
