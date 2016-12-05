71e9461347b04e1425a6fb718042b01eb5f5c4c8
puruscor <herra.melkor@gmail.com>
2016-12-05 23:19:56 +0200

    Hopefully fix NPE

--- src/haven/MapView.java
+++ src/haven/MapView.java
@@ -1035,8 +1035,9 @@ public class MapView extends PView implements DTarget, Console.Directory, PFList
         // This solution is bad but currently no better avaible
         if(!Config.hidesky) {
 	        boolean skyb = true;
-	        if(player()!=null && player().getc() !=null) {
-		        Coord pltc = new Coord((int)player().getc().x / 11, (int)player().getc().y / 11);
+	        Gob player = player();
+	        if(player!=null && player.getc() !=null) {
+		        Coord pltc = new Coord((int)player.getc().x / 11, (int)player.getc().y / 11);
 		        for (int x = -44; x < 44; x++) {
 		            for (int y = -44; y < 44; y++) {
 		                int t = glob.map.gettile(pltc.sub(x, y));
