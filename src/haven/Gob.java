3652fb6153f5c8ecd47245613a5d613446eb1747
puruscor <herra.melkor@gmail.com>
2017-01-14 00:16:29 +0200

    Added big palisade gate to exception list of hiding function

--- src/haven/Gob.java
+++ src/haven/Gob.java
@@ -507,7 +507,7 @@ public class Gob implements Sprite.Owner, Skeleton.ModOwner, Rendered {
                         	hide = true;
                     
                     if (Config.hidewalls) 
-                    	 if (res .name.startsWith("gfx/terobjs/arch/pali") &&  !res.name.equals("gfx/terobjs/arch/palisadegate") || res.name.startsWith("gfx/terobjs/arch/brick") && !res.name.equals("gfx/terobjs/arch/brickwallgate") || res.name.startsWith("gfx/terobjs/arch/pole") && !res.name.equals("gfx/terobjs/arch/polegate")) 
+                    	 if (res.name.startsWith("gfx/terobjs/arch/pali") &&  !res.name.equals("gfx/terobjs/arch/palisadegate") && !res.name.equals("gfx/terobjs/arch/palisadebiggate") || res.name.startsWith("gfx/terobjs/arch/brick") && !res.name.equals("gfx/terobjs/arch/brickwallgate") || res.name.startsWith("gfx/terobjs/arch/pole") && !res.name.equals("gfx/terobjs/arch/polegate")) 
                     		 hide = true;
                     
                     if (Config.hidewagons) 
