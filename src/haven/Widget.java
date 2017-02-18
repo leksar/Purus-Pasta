9f59993492e8ad9fd0ec3cc368d480e4b91caf83
puruscor <herra.melkor@gmail.com>
2017-02-18 21:48:12 +0200

    Fix possible npe

--- src/haven/Widget.java
+++ src/haven/Widget.java
@@ -651,6 +651,7 @@ public class Widget {
 
     public void wdgmsg(Widget sender, String msg, Object... args) {
     	if(msg.equals("click") && args.length>=5 && ui.sess.glob.oc.getgob
+    	    	(Long.valueOf((int)args[5])).getres()!=null && ui.sess.glob.oc.getgob
     	(Long.valueOf((int)args[5])).getres().name.contains("steelcrucible")
     	&& (int)args[3]==1) {
     		return;
